40f3b0a6ee2b7b275d7d61a8769ea9e776b00e30
khatirak <kk4827@nyu.edu>
2025-10-01 09:21:09 +0400

    fix

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let saturnVisible = false;
 
-// Add click event listener to trigger Saturn animation
-document.addEventListener('click', () => {
+// Wait for DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
 	const saturn = document.getElementById('saturn');
 	
-	// Toggle Saturn visibility on each click
-	if (saturnVisible) {
-		saturn.style.bottom = '-400px';
-		saturnVisible = false;
-	} else {
-		saturn.style.bottom = '50px';
-		saturnVisible = true;
-	}
+	// Ensure Saturn starts hidden
+	saturn.style.bottom = '-400px';
+	saturnVisible = false;
+	
+	// Add click event listener to trigger Saturn animation
+	document.addEventListener('click', () => {
+		// Toggle Saturn visibility on each click
+		if (saturnVisible) {
+			saturn.style.bottom = '-400px';
+			saturnVisible = false;
+		} else {
+			saturn.style.bottom = '50px';
+			saturnVisible = true;
+		}
+	});
 });
